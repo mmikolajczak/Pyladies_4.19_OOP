dbb95f00698677bf8fd55085448f1273b8c279ad
mmikolajczak <mkm0796@gmail.com>
2018-05-11 17:15:40 +0200

    comments for other tasks, minor fixes

--- zad_2.py
+++ zad_2.py
@@ -8,12 +8,13 @@ Zastanów się, jak powinna wyglądać ich hierarchia i właściwości, a nastę
 2.2
 Zgłosił się do nas wybitny japoński malarz pokojowy - Mazała Pisakim.
 Pomalowane przez niego pokoje budzą podziw dookoła całego globu i nie może opędzić się on od klientów.
-Z uwagi na dużą ilość zleceń postanowił spróbować zautomatyzować niektóre z wykonywanych przez siebie 
+Z uwagi na dużą ilość zleceń postanowił spróbować on zautomatyzować niektóre z wykonywanych przez siebie 
 podczas pracy czynności. Wybór Pana Mazały padł na liczenie powierzchni którą trzeba pomalować - 
 mierzenie powierzchni nadal będzie on wykonywał sam, jednak obliczenia na podstawie 
 wykonanych pomiarów chciałby on zautomatyzować przy wykorzystaniu jakiegoś programu.
-Przyjrzyj się plikowi dostarczonemu przez malarza i napisz następujące funkcje:
-- wczytującą dane o tym, jakie kształty trzeba pomalować z formatu w pliku 'do_pomalowania.txt',
-- obliczającą całkowitą powierzchnie do pomalowania z przekazanej do niej listy figur.
+Przyjrzyj się plikowi dostarczonemu przez malarza i napisz kod który:
+- wczytujuje dane o tym, jakie kształty trzeba pomalować z formatu w pliku 'do_pomalowania.txt 
+i tworzy z nich odpowiednie obiekty',
+- oblicza całkowitą powierzchnie do pomalowania na podstawie listy figur.
 (cóż za wspaniały zbieg okoliczności, że akurat zaimplementowaliśmy hierachię kształtów ^^).
 """
